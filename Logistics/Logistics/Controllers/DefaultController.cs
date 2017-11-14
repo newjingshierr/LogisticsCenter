701a4fb820f098e25bb54004c2d9c37947bfe7d4
enzo <jingshierr@126.com>
2017-11-14 19:10:45 +0800

    init swagger

---
+++ Logistics/Logistics/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Net;
+using System.Web.Http;
+
+namespace Logistics.Controllers
+{
+    public class DefaultController : ApiController
+    {
+
+        [HttpGet]
+        [Route("Items")]
+        public bool Items(int index = 0, int pageSize = 5)
+        {
+            return true;
+
+        }
+    }
+}
